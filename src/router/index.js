778bc4a1e24a808c960749ec8cbaadcccd0ed4f1
HellorWord <3100554714@qq.com>
2020-06-26 10:33:42 +0800

    no message

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import login from "@/components/index/login.vue";
+import header from "@/components/header/header.vue";
 Vue.use(VueRouter);
 
 const routes = [
@@ -8,6 +9,11 @@ const routes = [
     path: "/",
     name: "login",
     component:login
+  },
+  {
+    path: "/header",
+    name: "header",
+    component:header
   },
 ];
 
